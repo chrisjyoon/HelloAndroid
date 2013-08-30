0c10cc5a23c8e897fae2dd37b58f0bfccaa87ce2
chrisjyoon <chrisjyoon@gmail.com>
2013-08-30 17:56:43 +0900

    List Menu, ActionBar test

--- src/com/example/helloandroid/MainActivity.java
+++ src/com/example/helloandroid/MainActivity.java
@@ -12,29 +12,49 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 	public static final String DEBUG_TAG = "HelloExample";
 	EditText editText = null;
 	TextView textView = null;
+	SimpleAdapter mAdapter;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        editText = (EditText)findViewById(R.id.inputUri);
-        textView = (TextView)findViewById(R.id.textMessage);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, values);
+            setListAdapter(adapter);
     }
 
-    public void myClickHandler(View view) {
+    
+    
+    @Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+    	Log.d(DEBUG_TAG, "touch menu = " + position);
+		String item = (String)getListAdapter().getItem(position);
+		super.onListItemClick(l, v, position, id);
+	}
+
+
+
+	public void myClickHandler(View view) {
     	String stringUrl = editText.getText().toString();
     	ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
     	NetworkInfo ni = cm.getActiveNetworkInfo();
@@ -51,10 +71,22 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
     
-    private class DownloadTask extends AsyncTask<String, Void, String> {
+    @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_search:
+			Toast.makeText(this, "Do nothing...", Toast.LENGTH_SHORT).show();
+			break;
+		default:
+				return super.onOptionsItemSelected(item);
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private class DownloadTask extends AsyncTask<String, Void, String> {
 
 		@Override
 		protected String doInBackground(String... arg0) {
